8957d403c44eb42509e7aedcf3f120801063ed5b
gioguitos <giovannisevero10@gmail.com>
2018-03-25 20:05:53 -0300

    commit backup

--- WWDC2018/GameScene.swift
+++ WWDC2018/GameScene.swift
@@ -12,26 +12,40 @@ import GameplayKit
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var thief: SKSpriteNode!
-    var arrow: SKSpriteNode!
     var boss1: SKSpriteNode!
     var floor: SKSpriteNode!
+    var wall: SKSpriteNode!
+    
     var bossHp = 100
+    
     var inAir:Bool = false
+    var currentButton:Bool = false
+    var currentButton2:Bool = false
+    
+    
+    var textureAtlasRight = SKTextureAtlas()
+    var textureArrayRight = [SKTexture]()
+    var textureAtlasLeft = SKTextureAtlas()
+    var textureArrayLeft = [SKTexture]()
+    var textureAtlasJumpRight = SKTextureAtlas()
+    var textureArrayJumpRight = [SKTexture]()
+    var textureAtlasJumpLeft = SKTextureAtlas()
+    var textureArrayJumpLeft = [SKTexture]()
+    
+    var cavern = [SKSpriteNode]()
+    
+    
     
     
     private var activeTouches = [UITouch:String]()
-
-    
-    private var bossHitbox: SKSpriteNode!
-    private var attackHitbox: SKSpriteNode!
-    
     private var cameraMan: SKCameraNode?
-    
     
     
     let thiefCategory: UInt32 = 0x1 << 0
     let floorCategory: UInt32 = 0x1 << 1
     let bossCategory:  UInt32 = 0x1 << 2
+    
+    
     
     
     func delay(_ delay:Double, closure:@escaping ()->()) {
@@ -48,6 +62,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         thief.physicsBody?.categoryBitMask = thiefCategory
 
         thief.physicsBody?.contactTestBitMask = bossCategory
+        
+        thief.physicsBody?.allowsRotation = false
+        
+        thief.physicsBody?.restitution = 0
         
         
 
@@ -70,18 +88,51 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
 
     }
-    func setupFloor(){
+    func setupFloor(obj: SKSpriteNode){
 
-        floor.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: floor.size.width, height: floor.size.height))
-
-        floor.physicsBody?.usesPreciseCollisionDetection = true
+//        floor.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: floor.size.width, height: floor.size.height))
+//
+//        floor.physicsBody?.usesPreciseCollisionDetection = true
+//
+//        floor.physicsBody?.categoryBitMask = floorCategory
+//
+//        floor.physicsBody?.contactTestBitMask = thiefCategory
+//
+//        floor.physicsBody?.isDynamic = false
+//
+//        floor.physicsBody?.allowsRotation = false
+//
+//        floor.physicsBody?.restitution = 0
+        obj.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: obj.size.width, height: obj.size.height))
         
-        floor.physicsBody?.categoryBitMask = floorCategory
+        obj.physicsBody?.usesPreciseCollisionDetection = true
         
-        boss1.physicsBody?.contactTestBitMask = thiefCategory
-
-        floor.physicsBody?.isDynamic = false
+        obj.physicsBody?.categoryBitMask = floorCategory
+        
+        obj.physicsBody?.contactTestBitMask = thiefCategory
+        
+        obj.physicsBody?.isDynamic = false
+        
+        obj.physicsBody?.allowsRotation = false
+        
+        obj.physicsBody?.restitution = 0
     }
+    
+    func setupWall(){
+
+        wall.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: wall.size.width, height: wall.size.height))
+
+        wall.physicsBody?.usesPreciseCollisionDetection = true
+
+        wall.physicsBody?.categoryBitMask = floorCategory
+
+        wall.physicsBody?.contactTestBitMask = thiefCategory
+
+        wall.physicsBody?.isDynamic = false
+        
+        wall.physicsBody?.restitution = 0
+    }
+    
     
     func didBegin(_ contact: SKPhysicsContact) {
         let firstNode = contact.bodyA.node as! SKSpriteNode
@@ -89,10 +140,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         
         if firstNode.name == "thief" && secondNode.name == "boss"{
-            print("Hit")
             
             bossHp -= 50
-            print(bossHp)
             
             physicsWorld.gravity = CGVector(dx:0, dy:-5)
             
@@ -101,11 +150,80 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             }
         }
         
-        if secondNode.name == "floor" && firstNode.name == "thief"{
+        for i in 1...5{
+            if secondNode.name == "thief" && firstNode.name == cavern[i].name{
+                print(cavern[i])
+                inAir = false
             
-            print("Floor Hit")
+            
+            
+                thief.removeAction(forKey: "jumpRight")
+                thief.removeAction(forKey: "jumpLeft")
+                if currentButton == false{
+                    thief.removeAction(forKey: "animationRight")
+                    thief.texture = SKTexture(imageNamed: textureAtlasRight.textureNames[0])
+                
+                }
+                else{
+                    thief.action(forKey: "animationRight")
+                }
+            
+                if currentButton2 == false{
+                    thief.removeAction(forKey: "animationLeft")
+                    thief.texture = SKTexture(imageNamed: textureAtlasLeft.textureNames[0])
+                
+                }
+                else{
+                    thief.action(forKey: "animationLeft")
+                }
+            
+            
+            }
             
         }
+        
+        if secondNode.name == "floor" && firstNode.name == "thief"{
+            
+            inAir = false
+            
+            
+            
+            thief.removeAction(forKey: "jumpRight")
+            thief.removeAction(forKey: "jumpLeft")
+            if currentButton == false{
+                thief.removeAction(forKey: "animationRight")
+                thief.texture = SKTexture(imageNamed: textureAtlasRight.textureNames[0])
+
+            }
+            else{
+                thief.action(forKey: "animationRight")
+            }
+            
+            if currentButton2 == false{
+                thief.removeAction(forKey: "animationLeft")
+                thief.texture = SKTexture(imageNamed: textureAtlasLeft.textureNames[0])
+                
+            }
+            else{
+                thief.action(forKey: "animationLeft")
+            }
+            
+            
+        }
+        
+        if secondNode.name == "wall" && firstNode.name == "thief"{
+            
+            
+            inAir = false
+            
+            thief.texture = SKTexture(imageNamed: textureAtlasRight.textureNames[0])
+            
+            thief.removeAction(forKey: "jumpRight")
+            
+            
+            
+        }
+       
         
             
     }
@@ -116,45 +234,76 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         super.didMove(to: view)
         
+        textureAtlasRight = SKTextureAtlas(named: "right")
+        
+        for frames in 0...textureAtlasRight.textureNames.count - 1{
+            
+            let nameRight = "moving\(frames).png"
+            
+            textureArrayRight.append(SKTexture(imageNamed: nameRight))
+        }
+        
+        
+        
+        textureAtlasLeft = SKTextureAtlas(named: "left")
+        
+        for frames in 0...textureAtlasLeft.textureNames.count - 1{
+            
+            let nameLeft = "moving\(frames)left.png"
+            
+            textureArrayLeft.append(SKTexture(imageNamed: nameLeft))
+        }
+        
+        
+        
+        textureAtlasJumpRight = SKTextureAtlas(named: "jumpRight")
+        let nameJumpRight = "thiefJump.png"
+        textureArrayJumpRight.append(SKTexture(imageNamed: nameJumpRight))
+        
+        
+        
+        
+        
+        textureAtlasJumpLeft = SKTextureAtlas(named: "jumpLeft")
+        
+        for frames in 0...textureAtlasJumpLeft.textureNames.count {
+           
+            let nameJumpLeft = "jump\(frames)left.png"
+            
+            textureArrayJumpLeft.append(SKTexture(imageNamed: nameJumpLeft))
+        }
+        
+        
         thief = childNode(withName: "thief") as? SKSpriteNode
-
-        arrow = childNode(withName: "arrow") as? SKSpriteNode
        
         boss1 = childNode(withName: "boss") as? SKSpriteNode
         
         floor = childNode(withName: "floor") as? SKSpriteNode
         
-        attackHitbox = childNode(withName: "attackHitbox") as? SKSpriteNode
-
-        bossHitbox = childNode(withName: "bossHitbox") as? SKSpriteNode
+        wall = childNode(withName: "wall") as? SKSpriteNode
         
+        cameraMan = childNode(withName: "cameraMan") as? SKCameraNode
         
-        
-
-//        cameraMan = SKCameraNode()
-//        self.camera = cameraMan
-//        self.addChild(cameraMan!)
-
-
-//        buttonsSetup(newThief: buttonsSetup)
-//
-        
-        
+        for i in 1...6{
+            
+            cavern.append((childNode(withName: "rockmountain\(i)") as? SKSpriteNode)!)
+            
+        }
+        for i in 1...5{
+            print(cavern[i])
+            setupFloor(obj: cavern[i])
+        }
         
         physicsWorld.gravity = CGVector(dx:0, dy:-5)
+        
         physicsWorld.contactDelegate = self
         
-        print(thief.position.y)
-        
         floor.physicsBody?.affectedByGravity = false
-
-        
-        
-        
         
         setupThief()
         setupBoss1()
-        setupFloor()
+        setupFloor(obj: floor)
+        setupWall()
         
         
         
@@ -167,72 +316,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             activeTouches[touch] = button
             tapBegin(on: button)
         }
-        let bodies = thief.physicsBody?.allContactedBodies()
-        if (bodies?.isEmpty)!{
-            inAir = true
-        }
-        else{
-            inAir = false
-        }
-        
-
-//        let touch = touches.first
-//        let position = touch?.location(in: self)
-//        let touchNode = self.atPoint(position!)
-//
-//        if let name = touchNode.name{
-//            if name == "right"{
-//
-//                let moveRight = SKAction.moveBy(x: 10, y: 0, duration: 0.1)
-//                let moveRightForEver = SKAction.repeatForever(moveRight)
-//                thief.run(moveRightForEver, withKey: "right")
-//                clickRight = true
-////                thief.removeAction(forKey:"left")
-//            }
-//            if name == "left"{
-//                let moveLeft = SKAction.moveBy(x: -10, y: 0, duration: 0.1)
-//                let moveLeftForEver = SKAction.repeatForever(moveLeft)
-//                thief.run(moveLeftForEver, withKey: "left")
-//                clickLeft = true
-////                thief.removeAction(forKey:"right")
-//
-//
-//            }
-//            if name == "jump"{
-//                let jumpUpAction = SKAction.moveBy(x:0, y:100, duration:0.2)
-//                let jumpDownAction = SKAction.moveBy(x:0, y:-100, duration: 0.2)
-//                let jumpSequence = SKAction.sequence([jumpUpAction, jumpDownAction])
-//                thief.run(jumpSequence, withKey: "jump")
-//                clickJump = true
-//            }
-//            if name == "attack"{
-        
-                
-                
-//                 HitBoxes
-//                self.attackHitbox?.isHidden = false
-//                self.bossHitbox?.isHidden = false
-//
-//                attackHitbox.size = CGSize(width: 35, height: 35)
-//                bossHitbox.size = CGSize(width: 35, height: 35)
-//
-//                attackHitbox.position = CGPoint(x: thief.position.x + 60, y: thief.position.y + 20)
-//                bossHitbox.position = CGPoint(x: boss1.position.x - 120, y: thief.position.y + 20)
-//
-//                delay(0.5){
-//                    self.attackHitbox?.isHidden = true
-//                    self.bossHitbox?.isHidden = true
-//                }
-                
-                
-                
-                
-//                Remove o Boss
-//                boss1.removeFromParent()
-//
-//            }
-//        }
-    
     
     }
 
@@ -250,52 +333,166 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
 }
     private func tapBegin(on button: String) {
+       
+        
         if button == "right"{
             
-        let moveRight = SKAction.moveBy(x: 10, y: 0, duration: 0.1)
+        let moveRight = SKAction.moveBy(x: 30, y: 0, duration: 0.2)
+        
         let moveRightForEver = SKAction.repeatForever(moveRight)
+        
+        let animation = SKAction.animate(with: textureArrayRight, timePerFrame: 0.1)
+        
+        let animationForEver = SKAction.repeatForever(animation)
+        
+        currentButton = true
+        
         thief.run(moveRightForEver, withKey: "right")
-     
+            
+            if inAir == false{
+                
+                   thief.run(animationForEver, withKey: "animationRight")
+                
+                
+            }
+            
         }
         if button == "left"{
             
-            let moveRight = SKAction.moveBy(x: -10, y: 0, duration: 0.1)
-            let moveRightForEver = SKAction.repeatForever(moveRight)
-            thief.run(moveRightForEver, withKey: "left")
+            let moveLeft = SKAction.moveBy(x: -30, y: 0, duration: 0.1)
+            
+            let animation = SKAction.animate(with: textureArrayLeft, timePerFrame: 0.1)
+            
+            let animationForEver = SKAction.repeatForever(animation)
+            
+            let moveLeftForEver = SKAction.repeatForever(moveLeft)
+            
+            currentButton2 = true
+            
+            thief.run(moveLeftForEver, withKey: "left")
+            
+            if inAir == false{
+                    
+                thief.run(animationForEver, withKey: "animationLeft")
+
+                
+            }
+            
+            
+            
           
         }
+        print(currentButton)
+        print(currentButton2)
+        
+        if currentButton == true && currentButton2 == true{
+            thief.removeAction(forKey: "animationLeft")
+            thief.removeAction(forKey: "animationRight")
+            thief.texture = SKTexture(imageNamed: textureAtlasRight.textureNames[0])
+            
+        }
+        
+        
+        
         
         if button == "jump"{
             if inAir == false {
-                let jumpUpAction = SKAction.moveBy(x:0, y:100, duration:0.2)
-                let jumpDownAction = SKAction.moveBy(x:0, y:-100, duration: 0.2)
+            
+                let jumpUpAction = SKAction.moveBy(x:0, y:150, duration:0.2)
+                
+                let animation = SKAction.animate(with: textureArrayJumpRight, timePerFrame: 0.1)
+                
+                let animationForEver = SKAction.repeatForever(animation)
+                
+                let jumpDownAction = SKAction.moveBy(x:0, y:0, duration: 0.2)
+                
                 let jumpSequence = SKAction.sequence([jumpUpAction, jumpDownAction])
+                
+                
                 thief.run(jumpSequence, withKey: "jump")
-                print(thief.position.y)
+                
+                thief.run(animationForEver, withKey: "jumpRight")
+                
+                inAir = true
+                
+                
+                
             }
-            else{
-                print("ta no ar nao pode pular")
-            }
+            
             
  
         }
-        print(button)
+        
     }
     
     private func tapEnd(on button:String) {
         if button == "right"{
             thief.removeAction(forKey: "right")
+         
+            if inAir == false{
+                thief.texture = SKTexture(imageNamed: textureAtlasRight.textureNames[0])
+                thief.removeAction(forKey: "animationRight")
+              
+                
+            }
+            currentButton = false
         }
         if button == "left"{
             thief.removeAction(forKey: "left")
+          
+            
+            if inAir == false{
+                thief.texture = SKTexture(imageNamed: textureAtlasLeft.textureNames[0])
+                thief.removeAction(forKey: "animationLeft")
+                
+                
+            }
+            currentButton2 = false
         }
+        
+        if button == "right" || button == "left"{
+            if currentButton == true && currentButton2 == false{
+                
+                let animation = SKAction.animate(with: textureArrayRight, timePerFrame: 0.1)
+                
+                let animationForEver = SKAction.repeatForever(animation)
+                
+                thief.run(animationForEver, withKey: "animationRight")
+
+            
+            }
+        
+            if currentButton == false && currentButton2 == true{
+            
+
+            
+                let animation = SKAction.animate(with: textureArrayLeft, timePerFrame: 0.1)
+            
+                let animationForEver = SKAction.repeatForever(animation)
+            
+                thief.run(animationForEver, withKey: "animationLeft")
+            
+            
+            }
+        }
+        
+        if button == "jump"{
+            
+        }
+
+        print("End",currentButton)
+        print("End",currentButton2)
+        
+        
+        
+        
         
     }
     
     
     
     private func findButtonName(from touch: UITouch) -> String {
-        // replace this with your custom logic to detect a button location
+
         let location = touch.location(in: self.view)
         if location.x > (self.view?.frame.midX)! && location.y > (self.view?.frame.midY)!{
             return "right"
